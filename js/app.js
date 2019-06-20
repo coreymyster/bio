fabab841a0cb36db6cacba1e3d60805c74319de5
Corey Sokol <corey.sokol@c02vt5ddhtd6.corp.climate.com>
2019-06-20 15:19:36 -0500

    Added furniture website

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ let projects = [
   repoLink: 'https://github.com/coreymyster/memory-game'
   },
   {
-    img: 'img/png/memory-game-thumbnail.png',
-    name: 'Memory Game',
-    desc: 'A memory game where you have to match two of the same cards.',
-    url: 'projects/memory-game/index.html',
-    repoLink: 'https://github.com/coreymyster/memory-game'
+    img: 'img/png/furniture-thumbnail.png',
+    name: 'Wood Furniture Website',
+    desc: 'This website is built for a fictional custom wood furniture building company and was built using Handlebars JS.',
+    url: 'projects/furniture/index.html',
+    repoLink: 'https://github.com/coreymyster/wood-furniture-website'
   },
 ]
 
@@ -20,7 +20,7 @@ let projectItem = document.querySelector('.projectItem')
 for (let i = 0; i <= projects.length; i++) {
   projectItem.innerHTML += `
     <span>
-      <p><a href="${projects[i].url}"><img src="${projects[i].img}"></a></p>
+      <p><a href="${projects[i].url}"><img class="thumbnail" src="${projects[i].img}"></a></p>
       <p>${projects[i].name}</p>
       <p>${projects[i].desc}</p>
       <p class="links"><a href="${projects[i].url}">Link</a> | <a href="${projects[i].repoLink}" target="_blank"><img src="img/svg/github-icon.svg"></a></p>
